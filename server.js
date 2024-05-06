efc5ced916b946bde29d48342e8428bec7addd9e
DavidVocat <david.rappl@gmail.com>
2024-05-06 10:26:34 +0200

    Test 2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Test from David Vocat');
+	res.send('Test from David Vocat, neuer Test');
 });
 
 app.listen(PORT, HOST);
